d2554600ac686ad561f05a6b713c435c1fd77443
Temitope Olotin <temitope.olotin@andela.com>
2019-12-30 14:04:24 +0100

    Create twoSums.php

---
+++ twoSums.php
@@ -0,0 +1,21 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $indexes = [];
+        for($i = 0; $i < count($nums); $i++) {
+          for ($j = $i + 1; $j < count($nums); $j++) {
+            if (($nums[$i] + $nums[$j]) === $target) {
+              $indexes = [$i, $j];
+            }
+          }
+        }
+        return $indexes;
+    }
+    
+    
+}
